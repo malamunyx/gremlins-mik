10889927f70c7511ad989a2fddbb40ccd95d5540
malamunyx <mikaelindrawan138@gmail.com>
2022-09-17 11:01:32 +1000

    New player movement and collision method

--- src/main/java/gremlins/Player.java
+++ src/main/java/gremlins/Player.java
@@ -1,38 +1,67 @@
 package gremlins;
 
+// THIS IS WHAT WE USE TO MOVE RELEASE AND STOP AT A GRID
+//        if (!xMoving && (x % 20 == 0)) {
+//            xVel = 0;
+//        }
+//        if (!yMoving && (y % 20 == 0)) {
+//            yVel = 0;
+//        }
+////        this.moveHash = getHashIndex();
+//        x += xVel;
+//        y +=
+
+// CONSIDER USING THE HASHCODE TO DETERMINE.
+// HASHCODE: for int x, y,   hashcode = x/20 + width*(y/20)
+// x/20 and y/20 are integer divisions (Floor division)
+
 import processing.core.PImage;
 
 public class Player {
-    private final PImage img;
     private int x;
     private int y;
 
-    private int xVel = 0;
-    private int yVel = 0;
+    private int xVel;
+    private int yVel;
 
-    /*
-     * Determines where the next point of stopping
-     */
-    private int xMovePt; // will always start from starting pos of X.
-    private int yMovePt;
-    private boolean xMoving = false; // whether object is moving
+    private boolean xMoving = false;
     private boolean yMoving = false;
 
+    private int moveHashIdx;
 
     private static final int speed = 2;
 
-    public Player(PImage img, int x, int y) {
-        this.img = img;
+    public Player(int x, int y) {
         this.x = x;
         this.y = y;
-        this.xMovePt = x; // movePts start in the original pos.
-        this.yMovePt = y;
+        this.moveHashIdx = this.getHashIndex();
     }
 
-    public void draw(App app) {
+    public int getHashIndex() {
+        return (x / 20) + 36 * (y / 20);
+    }
+    public int getHashIndex(int x, int y) {
+        return (x / 20) + 36 * (y / 20);
+    }
+
+    public void draw(PImage img, App app) {
         app.image(img, x, y);
 
-        // check for collisions
+        // check for collisions, especially if tile is door etc.
+        if (xVel > 0 && app.mapTiles.containsKey(this.getHashIndex() + 1))
+            xMoving = false;
+
+        if (xVel < 0 && app.mapTiles.containsKey(this.getHashIndex() - 1))
+            xMoving = false;
+
+        if (yVel > 0 && app.mapTiles.containsKey(this.getHashIndex() + 36))
+            yMoving = false;
+
+        if (yVel < 0 && app.mapTiles.containsKey(this.getHashIndex() - 36))
+            yMoving = false;
+
+
+
 
         if (!xMoving && (x % 20 == 0)) {
             xVel = 0;
@@ -45,38 +74,36 @@ public class Player {
         y += yVel;
     }
 
-    public void left() {
-        xMoving = true;
-        if ((xMovePt - x)  == 2) // change for 1 pixel
-            xMovePt -= 20;
-        xVel = -speed;
+    public void left(App app) {
+        if (!app.mapTiles.containsKey(this.getHashIndex()-1)) {
+            xMoving = true;
+            xVel = -speed;
+        }
     }
 
-    public void right() {
-        xMoving = true;
-        if ((xMovePt - x)  == 2) // change for 1 pixel
-            xMovePt += 20;
-        xVel = speed;
+    public void right(App app) {
+        if (!app.mapTiles.containsKey(this.getHashIndex()+1)) {
+            xMoving = true;
+            xVel = speed;
+        }
     }
 
-    public void up() {
-        yMoving = true;
-        if ((yMovePt - y) == 2) // change for 1 pixel
-            yMovePt -= 20;
-        yVel = -speed;
+    public void up(App app) {
+        if (!app.mapTiles.containsKey(this.getHashIndex()-36)) {
+            yMoving = true;
+            yVel = -speed;
+        }
+
     }
 
-    public void down() {
-        yMoving = true;
-        if ((yMovePt + y)  == 2) // change for 1 pixel
-            yMovePt += 20;
-        yVel = speed;
-    }
-    public void yStop() {
-        yMoving = false;
+    public void down(App app) {
+        if (!app.mapTiles.containsKey(this.getHashIndex()+36)) {
+            yMoving = true;
+            yVel = speed;
+        }
     }
 
-    public void xStop() {
-        xMoving = false;
-    }
+    public void xStop() {xMoving = false;}
+
+    public void yStop() {yMoving = false;}
 }
