f0b19e23e7b5161c401551b69b939ee7768ad2cb
malamunyx <mikaelindrawan138@gmail.com>
2022-10-02 23:33:09 +1100

    Test folder and gradle setting amendments

---
+++ src/test/java/gremlins/appClassTest.java
@@ -0,0 +1,15 @@
+package gremlins;
+
+import processing.core.PApplet;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.*;
+
+public class appClassTest {
+
+    // getimage testing
+    @Test
+    public void getImageTestValidImage() {
+
+    }
+}
