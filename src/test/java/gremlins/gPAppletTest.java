7798ac1db2cc2b65c7c0dabcedfea0e0f0fe20af
malamunyx <mikaelindrawan138@gmail.com>
2022-10-20 20:10:05 +1100

    Updated the PApplet sketch testing cases

--- src/test/java/gremlins/gPAppletTest.java
+++ src/test/java/gremlins/gPAppletTest.java
@@ -9,7 +9,7 @@ import java.io.File;
 import static org.assertj.core.api.Assertions.*;
 import static processing.core.PApplet.loadJSONObject;
 
-public class zPAppletTest {
+public class gPAppletTest {
     /* PApplet Setup Function Testing */
 
     /*
@@ -27,7 +27,6 @@ public class zPAppletTest {
         assertThat(na.getLives()).isEqualTo(conf.getInt("lives"));
         assertThat(na.getMaxLevel()).isEqualTo(conf.getJSONArray("levels").size());
         assertThat(na.getLevel()).isEqualTo(1); // By default, start level at 1.
-        na.exit();
     }
 
     /* End Game Testing [INTEGRATION TESTING] */
@@ -84,10 +83,11 @@ public class zPAppletTest {
         na.getCurrentLevel().getPlayer().SetPosition(moveX, moveY);
         assertThat(na.getCurrentLevel().playerWin()).isTrue();
 
-        na.delay(1000);
+        na.delay(500);
         assertThat(na.getLevel()).isEqualTo(2);
     }
 
+    /* Firing Iceball */
     @Test
     public void EmptyMapIceballUp() {
         App na = new App();
@@ -152,7 +152,114 @@ public class zPAppletTest {
         na.delay(300);
     }
 
-//    @Test
-    
-//    public void
+    /* Firing Fireball */
+    @Test
+    public void EmptyMapFireballUp() {
+        App na = new App();
+        na.configPath = "TestConfig/EmptyTestLevelConfig.json";
+
+        na.loop();
+        PApplet.runSketch(new String[]{"App"}, na);
+        na.setup();
+        na.delay(200);
+        na.getCurrentLevel().getPlayer().SetPosition(40, 180);
+        na.keyCode = PApplet.UP;
+        na.keyPressed();
+        na.delay(100);
+        na.keyReleased();
+        na.getCurrentLevel().getPlayer().fire();
+        assertThat(na.getCurrentLevel().getSprites()).hasAtLeastOneElementOfType(Fireball.class);
+        na.delay(300);
+    }
+
+    @Test
+    public void EmptyMapFireballDown() {
+        App na = new App();
+        na.configPath = "TestConfig/EmptyTestLevelConfig.json";
+
+        na.loop();
+        PApplet.runSketch(new String[]{"App"}, na);
+        na.setup();
+        na.delay(200);
+        na.keyCode = PApplet.DOWN;
+        na.keyPressed();
+        na.delay(100);
+        na.keyReleased();
+        na.getCurrentLevel().getPlayer().fire();
+        assertThat(na.getCurrentLevel().getSprites()).hasAtLeastOneElementOfType(Fireball.class);
+
+        na.delay(300);
+    }
+
+    @Test
+    public void EmptyMapFireballLeft() {
+        App na = new App();
+        na.configPath = "TestConfig/EmptyTestLevelConfig.json";
+
+        na.loop();
+        PApplet.runSketch(new String[]{"App"}, na);
+        na.setup();
+        na.delay(200);
+        na.getCurrentLevel().getPlayer().SetPosition(180, 40);
+        na.keyCode = PApplet.LEFT;
+        na.keyPressed();
+        na.delay(100);
+        na.keyReleased();
+        na.getCurrentLevel().getPlayer().fire();
+        assertThat(na.getCurrentLevel().getSprites()).hasAtLeastOneElementOfType(Fireball.class);
+
+        na.delay(300);
+    }
+
+    @Test
+    public void EmptyMapFireballAndIceBallRight() {
+        App na = new App();
+        na.configPath = "TestConfig/EmptyTestLevelConfig.json";
+
+        na.loop();
+        PApplet.runSketch(new String[]{"App"}, na);
+        na.setup();
+        na.delay(200);
+        na.keyCode = PApplet.RIGHT;
+        na.keyPressed();
+        na.delay(100);
+        na.keyReleased();
+        na.getCurrentLevel().getPlayer().fire();
+        assertThat(na.getCurrentLevel().getSprites()).hasAtLeastOneElementOfType(Fireball.class);
+
+        na.delay(300);
+    }
+
+    /* Powerup integration sketch testing */
+    @Test
+    public void EmptyMapTestPowerup() {
+        App na = new App();
+        na.configPath = "TestConfig/EmptyTestLevelConfig.json";
+
+        na.loop();
+        PApplet.runSketch(new String[]{"App"}, na);
+        na.setup();
+        na.delay(200);
+        na.getCurrentLevel().getPlayer().SetPosition(60, 40);
+        ((Powerup)na.getCurrentLevel().getTile(75)).setCanEffect(true);
+        na.delay(300);
+        assertThat(na.getCurrentLevel().getPlayer().getPlayerSpeed()).isEqualTo(4);
+    }
+
+    @Test
+    public void playerShootBrickwall() {
+        App na = new App();
+        na.loop();
+        PApplet.runSketch(new String[]{"App"}, na);
+        na.setup();
+
+        na.delay(300);
+        na.getCurrentLevel().getPlayer().setDir('R');
+        na.getCurrentLevel().getPlayer().fire();
+        assertThat(na.getCurrentLevel().getTile(40)).isInstanceOf(Wall.class);
+        na.delay(700);
+        assertThat(((Wall)na.getCurrentLevel().getTile(40)).isBroken()).isTrue();
+    }
+
+
 }
